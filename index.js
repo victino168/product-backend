da985c045ba294415fa39cf0a184060b2185358b
victino168 <santacruise4sky@gmail.com>
2025-02-15 11:50:57 +0100

    .env .gitIgnore

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const productRoute = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes.js")
 const cors = require("cors");
+const dotenv = require("dotenv")
 
 app.use("/api/product", productRoute);
 app.use("/api/user", userRoutes);
@@ -18,7 +19,7 @@ app.listen(6000, () => {
 
 mongoose
   .connect(
-    "mongodb+srv://santacruise4sky:tuOILFwdET6zJw4W@cluster0.cm8jv.mongodb.net/"
+    
   )
   .then(() => {
     console.log("database connected");
